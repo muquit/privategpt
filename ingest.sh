186fe514d4be41c2b3ac70b9d873a28997569367
muquit <muquit@gmail.com>
2024-09-15 21:54:57 -0400

    pvenv is now at the base

--- ingest.sh
+++ ingest.sh
@@ -3,5 +3,5 @@
 # muquit@muquit.com Sep-14-2024 
 #==================================================================== 
 DIRNAME=$(dirname $0)
-source ${DIRNAME}/ingest/pvenv/bin/activate
-ingest/ingest.py
+source ${DIRNAME}/pvenv/bin/activate
+${DIRNAME}/ingest/ingest.py
