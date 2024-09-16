00f89869faf0eea75654c26ae4bf62792ece1d1f
muquit <muquit@gmail.com>
2024-09-15 23:59:27 -0400

    update

--- pyclean.sh
+++ pyclean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Muhammad.Muquit@cygnalabs.com Aug-12-2024 
+# muquit@muquit.com 
 find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -print
 find . -type f -name '*.py[co]' -delete -o -type d -name __pycache__ -delete
